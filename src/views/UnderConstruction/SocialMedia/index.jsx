ba5fd67617de3cdd09578d481e29787d87c4b2b5
Almer Tampus <almerflorestampus@gmail.com>
2022-04-30 22:48:31 +0800

    Set target to blank; Add pinterest link

--- src/views/UnderConstruction/SocialMedia/index.jsx
+++ src/views/UnderConstruction/SocialMedia/index.jsx
@@ -2,16 +2,20 @@
 import './style.scss';
 import github from '../../../assets/icons/github.png';
 import linkedin from '../../../assets/icons/linkedin.png';
+import pinterest from '../../../assets/icons/pinterest.png';
 
 // main component
 const SocialMedia = () => {
   return (
     <div className='social-media'>
-      <a href='https://github.com/guyfrommilkyway'>
+      <a href='https://github.com/guyfrommilkyway' target='_blank' rel='noreferrer'>
         <img className='icons' src={github} alt='github' />
       </a>
-      <a href='https://linkedin.com/in/almerflorestampus'>
+      <a href='https://linkedin.com/in/almerflorestampus' target='_blank' rel='noreferrer'>
         <img className='icons' src={linkedin} alt='linkedin' />
+      </a>
+      <a href='https://www.pinterest.ph/guyfrommilkyway' target='_blank' rel='noreferrer'>
+        <img className='icons' src={pinterest} alt='linkedin' />
       </a>
       <a href='https://www.flaticon.com/free-icons/github' title='github icons'>
         Github icons created by riajulislam - Flaticon
